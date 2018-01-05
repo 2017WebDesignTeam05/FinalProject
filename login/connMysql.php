d22975ba589f2eeaec2f281c5d40e6e106de94dc
leaflu0315 <johnson7511333@gmail.com>
2018-01-05 17:55:07 +0800

    updated

--- login/connMysql.php
+++ login/connMysql.php
@@ -1,10 +1,16 @@
 <?php 
 	//資料庫主機設定
-	$db_host = "140.123.101.175";
-	$db_username = "team05";
-	$db_password = "eggroll";
-	$db_table = "member";
-	include("Database.php");
-	
+	$host = "140.123.101.175";
+	$username = "team05";
+	$password = "eggroll";
+	$table = "member";
+	$conn = mysql_connect($host,$username,$password,$table);
+	if(!$conn){
+		die("Connection Failed :".mysql_connect_error());
+	}
+	else{
+		echo "Success";
+	}
 	mysql_query("SET NAMES 'utf8'");
+	
 ?>
